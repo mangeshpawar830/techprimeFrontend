783000cd0443360e163f603a1e91131c8ef21e99
Mangesh Chhatragun Pawar <105001146+mangeshpawar830@users.noreply.github.com>
2024-01-07 21:51:50 +0530

    Update project-list.component.ts

--- e2e/src/app/project-list/project-list.component.ts
+++ e2e/src/app/project-list/project-list.component.ts
@@ -7,7 +7,7 @@ import { ApiService } from '../api.service';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
-  constructor(private api:ApiService){}
+  constructor(private api: ApiService) {}
 
   public projectData: any[] = [];
 
@@ -19,36 +19,57 @@ export class ProjectListComponent implements OnInit {
     this.getProjects();
   }
 
-  public getProjects(): void {
+  public getProjects(searchTerm?: string, sortField?: string): void {
     this.api.project_lists().subscribe((res: any) => {
-    
+      if (searchTerm) {
+        res = res.filter((item: any) =>
+          item.proname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.projectReason.prname.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      }
+
+      if (sortField) {
+        res = res.sort((a: any, b: any) => {
+          switch (sortField) {
+            case 'project_priority':
+              return a.projectPriority.ppname.localeCompare(b.projectPriority.ppname);
+            case 'project_category':
+              return a.projectCategory.pcname.localeCompare(b.projectCategory.pcname);
+            case 'project_reason':
+              return a.projectReason.prname.localeCompare(b.projectReason.prname);
+            case 'project_division':
+              return a.projectDivision.pdivname.localeCompare(b.projectDivision.pdivname);
+            case 'project_department':
+              return a.projectDept.pdname.localeCompare(b.projectDept.pdname);
+            case 'project_location':
+              return a.projectLocation.plname.localeCompare(b.projectLocation.plname);
+            default:
+              return 0;
+          }
+        });
+      }
+
       this.projectData = res;
-      console.log(this.projectData);
-      console.log(res);
       this.totalProduct = res.length;
-    })
+    });
   }
 
- 
-
-
-  public changestatus(id:number,obj:any): void {
-
-    console.log("psid: "+id)
-    this.api.change_status(id,obj).subscribe((res: any)=>{
+  public changestatus(id: number, obj: any): void {
+    console.log("psid: " + id);
+    this.api.change_status(id, obj).subscribe((res: any) => {
       console.log(id);
-
       console.log(obj);
       this.getProjects();
-
-    })
+    });
   }
 
   onSearch() {
-    this.getProjects();
+    const searchTerm = (document.querySelector('.serch-input') as HTMLInputElement).value;
+    this.getProjects(searchTerm);
   }
 
   onSort() {
-    this.getProjects();
+    const sortField = (document.querySelector('.sort-box') as HTMLSelectElement).value;
+    this.getProjects(undefined, sortField);
   }
 }
